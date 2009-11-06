6612f0b2fdccad40246f18874611c02b44aaa9c3
Vidar Hokstad <vidar@hokstad.com>
2009-11-06 16:45:00 +0000

    Pass the data on

--- lib/hokstad-filters/formfilter.rb
+++ lib/hokstad-filters/formfilter.rb
@@ -6,5 +6,6 @@ class FormFilter < Filter
   def filter line, tag
     line.gsub!("[ ]","<input type='checkbox'/>")
     line.gsub!("[X]","<input type='checkbox' checked='checked' />")
+    pass(line,tag)
   end
 end
