bbd7c819dbef137a3cfbe5eb32339f44dd58a716
Vidar Hokstad <vidar@hokstad.com>
2009-11-12 22:38:38 +0000

    Added filter to handle strike-through

--- lib/hokstad-filters.rb
+++ lib/hokstad-filters.rb
@@ -3,7 +3,8 @@ require 'hokstad-filters/precodefilter'
 require 'hokstad-filters/markdownfilter'
 require 'hokstad-filters/rubyhighlighter'
 require 'hokstad-filters/formfilter'
+require 'hokstad-filters/inlinemarkupfilter'
 
 def extended_markdown(data)
-  Filter.chain(FormFilter,PreCodeFilter,RubyHighlighter,MarkdownFilter).process(data)
+  Filter.chain(FormFilter,PreCodeFilter,RubyHighlighter,InlineMarkupFilter,MarkdownFilter).process(data)
 end
---
+++ lib/hokstad-filters/inlinemarkupfilter.rb
@@ -0,0 +1,16 @@
+
+require 'hokstad-filters/filter'
+
+# Adds assorted inline markup options to Markdown
+class InlineMarkupFilter < Filter
+
+  def initialize n = nil
+    super
+  end
+
+  def filter line, tag = nil
+    line.gsub!(/~~?([a-zA-Z](\\~|[^~])*)~~?/,'<del>\1</del>')
+    line.gsub!('\~','~')
+    pass(line,tag)
+  end
+end
