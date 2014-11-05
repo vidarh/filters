b3ecc8dd3ccee72a38ae8bad835e4a026ca0d027
Vidar Hokstad <vidar@hokstad.com>
2014-11-05 01:47:57 +0000

    Changed to reflect trailing LF

--- spec/inlinemarkup_spec.rb
+++ spec/inlinemarkup_spec.rb
@@ -3,8 +3,8 @@ require 'hokstad-filters/inlinemarkupfilter'
 
 describe InlineMarkupFilter do
   it "takes a string of characters enclosed by '~' and surrounds them by <del/> tags" do
-    Filter.chain(InlineMarkupFilter).process("~abc~").should == "<del>abc</del>"
-    Filter.chain(InlineMarkupFilter).process("X~abc 123 - this is a test~foo").should == "X<del>abc 123 - this is a test</del>foo"
-    Filter.chain(InlineMarkupFilter).process("~2012-02-14~").should == "<del>2012-02-14</del>"
+    Filter.chain(InlineMarkupFilter).process("~abc~").should == "<del>abc</del>\n"
+    Filter.chain(InlineMarkupFilter).process("X~abc 123 - this is a test~foo").should == "X<del>abc 123 - this is a test</del>foo\n"
+    Filter.chain(InlineMarkupFilter).process("~2012-02-14~").should == "<del>2012-02-14</del>\n"
   end
 end
